18542e7bcac29f10fd58c70155ff42bead6cce44
Thanujadr <thanujathanut@gmail.com>
2023-05-29 11:17:17 +0530

    weather

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/Navbar';
-import Home from './component/Home';
 import Weather from './component/Weather';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App"> 
       <Weather/>
     </div>
   );
